a1b0ca69872e592eab5665847a1319c93658b7b4
j-prt <peart.l.joe@gmail.com>
2023-07-07 14:47:51 -0700

    add csv writer utility

--- scripts/webscraping/scrape.py
+++ scripts/webscraping/scrape.py
@@ -1,10 +1,25 @@
 """Program for performing various webscraping operations."""
 import argparse
 import json
+import csv
+from datetime import datetime, timezone
 from pathlib import Path
 from scrapers import ScrapeIt
 
+
+def write_csv(stem, articles):
+    """Write a collection of articles to a timestamped csv."""
+    now = datetime.now(timezone.utc).strftime('%Y%M%d%H%M')
+    with open('{}-{}.csv'.format(stem, now), 'w', newline='') as csvfile:
+        fieldnames = articles[0].keys()
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for row in articles:
+            writer.writerow(row)
+
 parser = argparse.ArgumentParser()
+scraper = ScrapeIt()
 
 parser.add_argument(
     'mode',
@@ -25,9 +40,16 @@ args = parser.parse_args()
 
 print(args)
 
+if args.checkpoint:
+    checkpoint_path = Path(args.checkpoint)
+    with open(checkpoint_path, 'r') as fp:
+        checkpoints = json.load(fp)
+    print(checkpoints)
+
 if args.mode == 'all':
     # TODO
-    pass
+    arxiv = scraper.arxiv()
+    write_csv('arxiv', arxiv)
 elif args.mode == 'fast':
     # TODO
     pass
