ba4b1950bbce40687530e01f79b7f82efc450355
j-prt <peart.l.joe@gmail.com>
2023-07-08 11:37:09 -0700

    add more scraping scripts

---
+++ scripts/webscraping/scrape-fastest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~/Desktop/projects/python/pb/scripts/webscraping
+source venv/bin/activate
+
+python3 scrape.py fastest
+
+gcloud auth login --cred-file=$CRED_FILE
+gcloud storage cp --recursive ./*.csv $ARTICLE_BUCKET
+gcloud auth revoke --all
+
+sudo mkdir -p ~/Desktop/archive/articles/$(date +%s)
+sudo mv ./*.csv $_
+deactivate
