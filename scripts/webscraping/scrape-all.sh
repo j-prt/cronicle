e1a612578b4ca7cc55f49198c2108c22b58c7426
j-prt <peart.l.joe@gmail.com>
2023-07-08 11:15:14 -0700

    add shell script for scraping

---
+++ scripts/webscraping/scrape-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/Desktop/projects/python/pb/scripts/webscraping
+source venv/bin/activate
+
+python3 scrape.py all
+
+gcloud auth login —cred-file=$CRED_FILE
+gcloud storage cp --recursive ./*.csv $ARTICLE_BUCKET
+
+sudo mkdir -p ~/Desktop/archive/articles/$(date +%s)
+sudo mv ./*.csv $_
+deactivate
