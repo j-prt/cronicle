3d900e4631403c5a08b6f71b541bbed72007d7e4
j-prt <peart.l.joe@gmail.com>
2023-07-11 17:00:48 -0700

    clean up unused imports/code

--- scripts/etl/articles_to_bq.py
+++ scripts/etl/articles_to_bq.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import re
 import csv
 import io
 
@@ -8,17 +7,6 @@ import io
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
-# def get_csv_reader(readable_file):
-
-#     # Open a channel to read the file from GCS
-#     gcs_file = beam.io.filesystems.FileSystems.open(readable_file)
-#     print(gcs_file)
-#     # Read file as a CSV
-#     gcs_reader = csv.reader(io.TextIOWrapper(gcs_file))
-#     print(gcs_reader)
-#     next(gcs_reader)
-
-#     return gcs_reader
 
 def read_csv_file(file):
     with beam.io.filesystems.FileSystems.open(file) as gcs_file:
@@ -45,21 +33,19 @@ def run(argv=None):
                         dest='output',
                         required=False,
                         help='Output title',
-                        default='test.txt')
+                        default='test')
 
     known_args, pipeline_args = parser.parse_known_args(argv)
 
-    # data_ingestion = DataIngestion()
 
     p = beam.Pipeline(options=PipelineOptions(pipeline_args))
     beam.io.WriteToText
-    result = (p
-            | 'Load url' >> beam.Create([known_args.input])
-            | 'Read csv' >> beam.FlatMap(read_csv_file)
-            # | 'Do some map' >> beam.Map(lambda row: row.split(','))
-            | 'String to text file' >> beam.io.WriteToText(known_args.output)
+    (p
+         | 'Load url' >> beam.Create([known_args.input])
+         | 'Read csv' >> beam.FlatMap(read_csv_file)
+         | 'String to text file' >> beam.io.WriteToText(known_args.output)
     )
-    p.run()
+    p.run().wait_until_finish()
 
 
 if __name__ == '__main__':
