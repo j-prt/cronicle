9bbd0814beb7a211bce8db517c8a517af576100a
j-prt <peart.l.joe@gmail.com>
2023-07-18 20:29:42 -0700

    reconfigure as a package for gcf - working with dataflow runner

--- scripts/etl/articles_to_bq.py
+++ scripts/etl/articles_to_bq.py
@@ -1,16 +1,5 @@
-import argparse
 import logging
-import json
-import ast
-import csv
 import os
-import io
-
-
-import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.io.gcp.bigquery_tools import parse_table_schema_from_json
-
 
 CLOUD_PROJECT=os.environ['CLOUD_PROJECT']
 TEMP_LOCATION=os.environ['TEMP_LOCATION']
@@ -19,84 +8,13 @@ TABLE=os.environ['TABLE']
 SCHEMA_FILE=os.environ['SCHEMA_FILE']
 
 
-class DataPreparation:
-    """Utility class for data transformations"""
-
-    # Used for holding dictionary keys (field names). Set as
-    # a class attribute because the individual instance state doesn't
-    # seem to be preserved during pipeline execution
-    table_keys = []
-
-    def read_csv_file(self, file):
-        """Function for reading csv files. Used instead of
-        the builtin beam csv reader due to newlines in fields."""
-        import apache_beam as beam
-        import csv
-        import io
-
-        with beam.io.filesystems.FileSystems.open(file) as gcs_file:
-            reader = csv.reader(io.TextIOWrapper(gcs_file))
-
-            # Get keys from header
-            table_keys = next(reader)+['create_time']
-            DataPreparation.table_keys.extend(table_keys)
-
-            # Read in the rows
-            for row in reader:
-                yield row
-
-    def get_schema(self, schema_file, source_site):
-        """Function for getting the appropriate schema for the
-        supplied csv file, based on the filename."""
-        import apache_beam as beam
-        import json
-
-        with beam.io.filesystems.FileSystems.open(schema_file) as f:
-            all_schemas = json.load(f)
-            target_schema = json.dumps(all_schemas[source_site])
-            schema = parse_table_schema_from_json(target_schema)
-        return schema
-
-    def parse_filename(self, filename):
-        """Function for both getting the article source name (to
-        match schemas) as well as parsing datetimes to bq format."""
-
-        filename = filename.split('/')[-1]
-        source_site = filename.split('-')[0]
-        timestamp = filename.split('-')[1].split('.')[0]
-        timestamp = (
-            timestamp[:4]
-            + '-'
-            + timestamp[4:6]
-            + '-'
-            + timestamp[6:8]
-            + 'T'
-            + timestamp[8:10]
-            + ':00:00'
-        )
-        return source_site, timestamp
-
-    def row_to_dict(self, row, keys, source_site, timestamp):
-        """Function for dealing with corner cases: where
-        fields need to be uploaded as repeated arrays in bq."""
-        import ast
-
-        if source_site == 'hackernews':
-            comments = ast.literal_eval(row.pop())
-            comments = [{'comment':item} for item in comments]
-            return dict(zip(keys, row+[comments]+[timestamp]))
-
-        if source_site == 'rogerebert':
-            tags = ast.literal_eval(row.pop())
-            tags = [{'tag':item} for item in tags]
-            return dict(zip(keys, row+[tags]+[timestamp]))
-
-        # Simple case (no repeated fields)
-        return dict(zip(keys, row+[timestamp]))
-
-
 def run(argv=None):
     """The main function which creates the pipeline and runs it."""
+    import argparse
+    import apache_beam as beam
+    from apache_beam.options.pipeline_options import PipelineOptions
+
+    from my_utils.dataprep import DataPreparation
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
